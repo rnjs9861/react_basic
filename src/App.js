4ea6cdf0c8758e4685c0608cbcb9db7375bf197f
rnjs9861 <rkwkvlrmakfh@gmail.com>
2024-04-19 13:16:14 +0900

    docs: 리액트 라우터 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import MainPage from "./pages/MainPage";
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
-    <>
-      <MainPage />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profiles/:username" element={<Profile />} />
+    </Routes>
   );
 }
 export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>소개페이지 입니다.</h1>
+    </div>
+  );
+}
+
+export default About;
